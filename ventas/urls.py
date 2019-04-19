5ccfde2d838731a58527283ef4cc5e7857cdedbd
landersoft <landersoft@gmail.com>
2019-04-19 18:41:14 -0300

    se agrega rutas absolutas y rutas urls

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,7 +1,15 @@
 from . import views
+#from ventas.views import nueva,index
 from django.urls import path, include
+
 
 urlpatterns = [
     path(r'', views.index, name='index'),
+    path(r'nueva/', views.nueva, name='nueva'),
+    path(r'nueva/boleta/', views.boleta, name='boleta'),
+    path(r'nueva/boleta/add', views.detalleadd, name='add'),
+    path(r'nueva/factura/', views.factura, name='factura'),
+    path(r'nueva/factura/add', views.detalleadd, name='add'),
+    path(r'nueva/boleta/boletalista/',views.VentaList.as_view()),
 
 ]
