960600ecd790c7d7acdaa54fa22fbb22ee4b9286
landersoft <landersoft@gmail.com>
2019-04-19 10:57:11 -0300

    se agregan el custom admin

--- abastecimiento/admin.py
+++ abastecimiento/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from modulos.abastecimiento.models import Producto, Proveedor, Compra, DetalleCompra
 
-# Register your models here.
+
+###########################################################################
+class AdminProveedor(admin.ModelAdmin):
+    list_display = ["rut", "nombre", "direccion", "telefono", "email"]
+    list_filter = ["rut"]
+    search_fields = ["rut", "nombre"]
+
+    class Meta:
+        model = Proveedor
+
+#############################################################################
+class AdminProducto(admin.ModelAdmin):
+    list_display = ["id", "nombre", "descripcion", "precio"]
+    list_filter = ["id","nombre"]
+    search_fields = ["id", "nombre"]
+
+    class Meta:
+        model = Producto
+
+##############################################################################
+class AdminCompra(admin.ModelAdmin):
+    list_display = ["id", "fecha", "proveedor", "total"]
+    list_filter = ["id","fecha"]
+    search_fields = ["id", "proveedor"]
+
+    class Meta:
+        model = Compra
+###############################################################################
+class AdminDetalleCompra(admin.ModelAdmin):
+    list_display = ["id_compra", "id_producto", "cantidad", "precio_unitario"]
+    list_filter = ["id_compra","id_producto"]
+    search_fields = ["id_compra"]
+
+    class Meta:
+        model = DetalleCompra
+
+
+
+
+admin.site.register(Proveedor, AdminProveedor)
+admin.site.register(Producto, AdminProducto)
+admin.site.register(Compra, AdminCompra)
+admin.site.register(DetalleCompra, AdminDetalleCompra)
+
