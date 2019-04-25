5732242d9fa991d7c160d8c49d892e6c94869e62
Rodrigo Eduardo Lander Sepulveda <Rodrigo.Lander@BUPA.CL>
2019-04-25 17:56:41 -0400

    cambio en database

--- AMASADOSUR/settings.py
+++ AMASADOSUR/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'AMASADOSUR.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'amasadosur',
+            'USER': 'root',
+            'PASSWORD': 'REIME94',
+            'HOST': 'localhost',
+            'PORT': '3306',
+         }
+     }
 
 
 # Password validation
